ecedb15a62241ac04cb82880d89d1e297540e19c
Triority <triority@qq.com>
2023-04-12 00:33:13 +0800

    改进了图片数量限制，添加了路径配置变量，并写了一些readme

--- auto_marking.py
+++ auto_marking.py
@@ -4,8 +4,17 @@ from txt_output import *
 import os
 import multiprocessing
 
-total = 100
-processes = 16
+# 每张图片与背景组合生成的图片总数
+# 如果设置数量过小(if total < processes * len(backs))将导致舍弃部分背景图片
+# 生成的图片总量 = imgs * processes * total
+total = 1
+# 使用的进程数，不大于cpu核心数，这里保留一个核心空闲避免电脑过于卡顿
+processes = multiprocessing.cpu_count() - 1
+# 图片路径设置
+backs_dir_path = r'C:\auto_marking\backs'
+images_dir_path = r'C:\auto_marking\images'
+marked_dir_path = r'C:\auto_marking\images_marked'
+output_dir_path = r'C:\auto_marking\output'
 
 
 def data_marker(img, img_marked, back):
@@ -13,7 +22,7 @@ def data_marker(img, img_marked, back):
     img = random_brightness(img)
     back = random_brightness(back)
     # 缩放，可自定义参数
-    r = random.randint(5, 25) / 100
+    r = random.randint(5, 75) / 100 * min(back.shape[0]/img.shape[0], back.shape[1]/img.shape[1])
     img = cv2.resize(img, (0, 0), fx=r, fy=r, interpolation=cv2.INTER_NEAREST)
     img_marked = cv2.resize(img_marked, (0, 0), fx=r, fy=r, interpolation=cv2.INTER_NEAREST)
     # 透视变换，可自定义参数
@@ -32,25 +41,36 @@ def data_marker(img, img_marked, back):
 
 def data_maker(a, pro):
     try:
-        m = 0
-        objs = os.listdir('auto_marking/images')
-        backs = os.listdir('auto_marking/backs')
+        objs = os.listdir(images_dir_path)
+        backs = os.listdir(backs_dir_path)
         data_format = 'yolo'
-        while m < pro:
-            for k in objs:
-                imgs = os.listdir('auto_marking/images/'+k)
-                for i in imgs:
-                    img = cv2.imread('auto_marking/images/' + k + '/' + i)
-                    img_marked = cv2.imread('auto_marking/images_marked/' + k + '/' + i)
+        for k in objs:
+            imgs = os.listdir(images_dir_path + '\\' + k)
+            if not os.path.exists(output_dir_path + "\\images\\" + k):
+                print('mkdir:'+output_dir_path + "\\images\\" + k)
+                os.makedirs(output_dir_path + "\\images\\" + k)
+            if not os.path.exists(output_dir_path + "\\images_marked\\" + k):
+                print('mkdir:'+output_dir_path + "\\images_marked\\" + k)
+                os.makedirs(output_dir_path + "\\images_marked\\" + k)
+            if not os.path.exists(output_dir_path + "\\labels\\" + k):
+                print('mkdir:'+output_dir_path + "\\labels\\" + k)
+                os.makedirs(output_dir_path + "\\labels\\" + k)
+            for i in imgs:
+                m = 0
+                while m < total:
+                    img = cv2.imread(images_dir_path + '\\' + k + '\\' + i)
+                    img_marked = cv2.imread(marked_dir_path + '\\' + k + '\\' + i)
                     for j in backs:
+                        if m > total:
+                            break
                         m += 1
                         print(k+' '+i+' '+j+' '+str(m))
                         s = str(m) + str(a)
-                        back = cv2.imread('auto_marking/backs/' + j)
+                        back = cv2.imread(backs_dir_path + '\\' + j)
                         data_output, xmin, ymin, xmax, ymax = data_marker(img, img_marked, back)
-                        cv2.imwrite("auto_marking/output/images/" + k + '/' + s + '.jpg', data_output)
+                        cv2.imwrite(output_dir_path + "\\images\\" + k + '\\' + s + '.jpg', data_output)
                         cv2.rectangle(data_output, (xmin, ymin), (xmax, ymax), (0, 255, 1), 2)
-                        cv2.imwrite("auto_marking/output/images_marked/" + k + '/' + s + '.jpg', data_output)
+                        cv2.imwrite(output_dir_path + "\\images_marked\\" + k + '\\' + s + '.jpg', data_output)
                         if data_format == 'voc':
                             picture_width = back.shape[1]
                             picture_height = back.shape[0]
@@ -62,7 +82,7 @@ def data_maker(a, pro):
                             label_name = s + '.txt'
                         else:
                             raise Exception('wrong label_name')
-                        path = 'auto_marking/output/labels/' + k + '/' + label_name
+                        path = output_dir_path + '\\labels\\' + k + '\\' + label_name
                         fw = open(path, 'w')
                         fw.write(txt)
                         fw.close()
